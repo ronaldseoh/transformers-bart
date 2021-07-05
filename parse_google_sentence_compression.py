15db099253eb39ae31d422c08abad7efada57029
Ronald Seoh <ronaldseoh@icloud.com>
2021-07-05 16:13:57 -0400

    Update parse_google_sentence_compression.py

--- parse_google_sentence_compression.py
+++ parse_google_sentence_compression.py
@@ -1,38 +1,47 @@
+import gzip
 import json
-import csv
-from glob import glob
+import glob
 
-fieldnames = ['original','compressed']
 
-def to_csv_record(writer, buffer):
-  record = json.loads(buffer)
-  writer.writerow(dict(
-    original=record['graph']['sentence'],
-    compressed=record['compression']['text']))
+def write_to_file(source_file, target_file, temp_buffer):
 
-with open('training_data.csv','w') as csvfile:
-  writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-  writer.writeheader()
+    record = json.loads(temp_buffer)
 
-  for json_file in glob('**train**.json'):
-    with open(json_file) as raw_contents:
-      buffer = ''
-      for line in raw_contents:
-        if line.strip()=='':
-            to_csv_record(writer, buffer)
-            buffer = ''
-        else: buffer += line
-      if len(buffer)>0: to_csv_record(writer, buffer)
+    source_file.write(record['graph']['sentence'] + '\n')
+    target_file.write(record['compression']['text'] + '\n')
 
-with open('eval_data.csv','w') as csvfile:
-  writer = csv.DictWriter(csvfile, fieldnames=['original','compressed'])
-  writer.writeheader()
-  
-  with open('comp-data.eval.json') as raw_contents:
-    buffer = ''
+train_source = open('train.source','w')
+train_target = open('train.target','w')
+
+for json_file in glob.glob('**train**.json.gz'):
+    with gzip.open(json_file, 'rt') as raw_contents:
+        temp_buffer = ''
+        for line in raw_contents:
+            if line.strip() =='':
+                write_to_file(train_source, train_target, temp_buffer)
+                temp_buffer = ''
+            else: temp_buffer += line
+
+        if len(temp_buffer) > 0:
+            write_to_file(train_source, train_target, temp_buffer)
+
+train_source.close()
+train_target.close()
+
+dev_source = open('dev.source','w')
+dev_target = open('dev.target','w')
+
+with gzip.open('comp-data.eval.json.gz', 'rt') as raw_contents:
+    temp_buffer = ''
+
     for line in raw_contents:
-      if line.strip()=='':
-          to_csv_record(writer, buffer)
-          buffer = ''
-      else: buffer += line
-    if len(buffer)>0: to_csv_record(writer, buffer)
+        if line.strip() == '':
+            write_to_file(dev_source, dev_target, temp_buffer)
+            temp_buffer = ''
+        else: temp_buffer += line
+
+    if len(temp_buffer) > 0:
+        write_to_file(dev_source, dev_target, temp_buffer)
+
+dev_source.close()
+dev_target.close()
